d9706db64aa9a96d44dcb424ebc267966d8e3026
Milind123456 <milindbasavaraja96@gmail.com>
2017-06-06 22:50:24 +0530

    Adding OnItemClickListener

--- app/src/main/java/com/example/test/listview/MainActivity.java
+++ app/src/main/java/com/example/test/listview/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.test.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
     ListView l;
     String[] days ={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
     @Override
@@ -15,5 +19,13 @@ public class MainActivity extends AppCompatActivity {
         l = (ListView)findViewById(R.id.listview);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,days);
         l.setAdapter(adapter);
+        l.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        TextView temp = (TextView)view;
+        Toast.makeText(this,temp.getText()+" "+position,Toast.LENGTH_SHORT).show();
     }
 }
