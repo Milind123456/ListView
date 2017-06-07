431b343bd89e0eeffea9be8dcb69b300c6ac4fd7
Milind123456 <milindbasavaraja96@gmail.com>
2017-06-08 01:03:37 +0530

    Advanced version of images with text in a list

--- app/src/main/java/com/example/test/listview/MainActivity.java
+++ app/src/main/java/com/example/test/listview/MainActivity.java
@@ -1,31 +1,72 @@
 package com.example.test.listview;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+public class MainActivity extends AppCompatActivity {
+
+    String[] memeTitles;
+    String[] memeDescription;
+    int[] images = {R.drawable.p1,R.drawable.p2,R.drawable.p3,R.drawable.p4,R.drawable.p1,R.drawable.p2,R.drawable.p3,R.drawable.p4};
     ListView l;
-    String[] days ={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        l = (ListView)findViewById(R.id.listview);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.single_row,R.id.textView,days);
+
+        Resources res = getResources();
+        memeTitles = res.getStringArray(R.array.titles);
+        memeDescription = res.getStringArray(R.array.descriptions);
+        l = (ListView) findViewById(R.id.listview);
+        milAdapter adapter = new milAdapter(this,memeTitles,images,memeDescription);
         l.setAdapter(adapter);
-        l.setOnItemClickListener(this);
+
     }
 
 
+}
+
+class milAdapter extends ArrayAdapter<String>{
+    Context context;
+    int[] images;
+    String[] titleArray;
+    String[] descriptionArray;
+    milAdapter(Context c , String[] titles,int imgs[],String[] desc){
+        super(c,R.layout.single_row,R.id.textView,titles);
+        this.context = c;
+        this.images = imgs;
+        this.titleArray = titles;
+        this.descriptionArray = desc;
+    }
+
+    @NonNull
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        TextView temp = (TextView)view;
-        Toast.makeText(this,temp.getText()+" "+position,Toast.LENGTH_SHORT).show();
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.single_row,parent,false);
+
+        ImageView image = (ImageView) row.findViewById(R.id.imageView);
+        TextView myTitle = (TextView) row.findViewById(R.id.textView);
+        TextView myDescription = (TextView) row.findViewById(R.id.textView2);
+
+        image.setImageResource(images[position]);
+        myTitle.setText(titleArray[position]);
+        myDescription.setText(descriptionArray[position]);
+
+        return row;
     }
 }
